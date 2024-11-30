caad2a263d5589d9705d650a21f191a7123290fd
Ronak-P99 <rocky101299@gmail.com>
2024-11-30 14:32:24 -0600

    eight commit

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def init_customers_info_data():
             ]
             customersAccounts = [
                 CustomerAccount(username="ctm1", password=generate_password_hash("password1"),customer_id=1),
-                CustomerAccount(username="ctm2", password=generate_password_hash("password2"),customer_id=2),
+                CustomerAccount(username="string", password=generate_password_hash("string"),customer_id=2),
                 CustomerAccount(username="ctm3", password=generate_password_hash("password3"),customer_id=3),
             ]
             products = [
